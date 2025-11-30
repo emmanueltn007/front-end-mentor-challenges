1e85e7d14e45a8a45b4a1450970bb527c7a171ea
Thembelihle Notyhawe <emmanuelnotyhawe@gmail.com>
2025-11-30 15:46:18 +0200

    added better javascript code

--- html-css-and-js/age-calculator-app/script.js
+++ html-css-and-js/age-calculator-app/script.js
@@ -8,61 +8,78 @@ const submitBtn = document.querySelector('#js-button');
 const userYearsEl = document.querySelector('#js-user-years');
 const userMonthsEl = document.querySelector('#js-user-months');
 
-let userAge = null;
-let userMonths = null;
-
-const userDayInput = parseFloat(dayInput.value);
-const userMonthInput = parseFloat(monthInput.value);
-const userYearInput = parseFloat(yearInput.value);
 const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1;
 const currentDay  = new Date().getDate();
 
-// CALCULATIONS
-const age = currentYear - userYearInput;
-const userCurrentMonths = 12 - userMonthInput; 
-
 // FUNCTIONS
 function validations () {
+    let valid = true;
     // CONDITIONS FOR YEARS
-    if (yearInput.value > currentYear) {
-        yearsAlert.textContent = 'Must be in the past';
-    }
-    else if (yearInput.value === '') {
+    if (yearInput.value === '') {
         yearsAlert.textContent = 'This field is required';
+        valid = false;
+    }
+    else if (yearInput.value > currentYear) {
+        yearsAlert.textContent = 'Must be in the past';
+        valid = false;
     }
     else {
         yearsAlert.textContent = '';
     }
 
     // CONDITIONS FOR MONTHS 
-    if (monthInput.value > 12 || monthInput.value < 1) {
-        monthsAlert.textContent = 'Must be a valid month';
-    }
-    else if (monthInput.value === '') {
+    if (monthInput.value === '') {
         monthsAlert.textContent = 'This field is required';
+        valid = false;
+    }
+    else if (monthInput.value < 1 || monthInput.value > 12) {
+        monthsAlert.textContent = 'Must be a valid month';
+        valid = false;
     }
     else {
         monthsAlert.textContent = '';
     }
 
     // CONDITIONS FOR DAYS  
-    if (dayInput.value < 1 || dayInput.value > 31) {
-        daysAlert.textContent = 'Must be a valid date';
-    }
-    else if (dayInput.value === '') {
+    if (dayInput.value === '') {
         daysAlert.textContent = 'This field is required';
+        valid = false;
+    }
+    else if (dayInput.value < 1 || dayInput.value > 31) {
+        daysAlert.textContent = 'Must be a valid date';
+        valid = false;
     }
     else {
         daysAlert.textContent = '';
     }
+
+    return valid;
 }
 
+// AGE CALCULATION
 function calculateAge () {
-    userYearsEl.textContent = age;
-}
+    const userDayInput = parseFloat(dayInput.value);
+    const userMonthInput = parseFloat(monthInput.value);
+    const userYearInput = parseFloat(yearInput.value);
 
-calculateAge();
+    let years = currentYear - userYearInput;
+    let months = (currentYear - userYearInput) * 12 + currentMonth - userMonthInput -12; 
+    let monthsPassed = months % 12;
+    let daysPassed = 
+
+    if (currentMonth < userMonthInput) {
+        years--;
+    }
+
+    if (currentDay >= userDayInput) {
+        monthsPassed++;
+    }
+
+    userYearsEl.textContent = years;
+    userMonthsEl.textContent = monthsPassed;
+    userDaysEl.textContent = daysPassed;
+}
 
 submitBtn.addEventListener('click', () => {
     validations();
