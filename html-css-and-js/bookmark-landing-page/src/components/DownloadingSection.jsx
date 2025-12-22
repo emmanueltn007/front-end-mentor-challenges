6ce1153afe67d14ea10fa4136f9725407e2f3f85
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-22 12:24:21 +0200

    Made Adjustments of the layout in Downloading Section

--- html-css-and-js/bookmark-landing-page/src/components/DownloadingSection.jsx
+++ html-css-and-js/bookmark-landing-page/src/components/DownloadingSection.jsx
@@ -33,7 +33,7 @@ function DownloadingSection () {
                     </div>
                 </div>
                 <div className="shadow-md shadow-gray-500 flex flex-col gap-8 items-center rounded-md p-4 md:absolute
-                                left-[50%] -translate-x-[50%] translate-y-8">
+                                md:left-[50%] md:-translate-x-[50%] md:translate-y-8">
                     <div className="flex flex-col items-center gap-4 pt-8">
                         <img 
                             className="w-20"
@@ -41,7 +41,7 @@ function DownloadingSection () {
                             alt="firefox-logo"
                         />
                         <h3 className="text-md text-[hsl(229,31%,21%)] font-semibold">
-                            Add to Chrome
+                            Add to Firefox
                         </h3>
                         <p className="text-gray-500 font-medium">
                             Minimum version 55
@@ -54,8 +54,8 @@ function DownloadingSection () {
                         </button>
                     </div>
                 </div>
-                <div className="shadow-md shadow-gray-500 flex flex-col gap-8 items-center rounded-md p-4 md:absolute right-0
-                                translate-y-16">
+                <div className="shadow-md shadow-gray-500 flex flex-col gap-8 items-center rounded-md p-4 md:absolute md:right-0
+                                md:translate-y-16">
                     <div className="flex flex-col items-center gap-4 pt-8">
                         <img 
                             className="w-20"
@@ -63,7 +63,7 @@ function DownloadingSection () {
                             alt="opera-logo"
                         />
                         <h3 className="text-md text-[hsl(229,31%,21%)] font-semibold">
-                            Add to Firefox
+                            Add to Opera
                         </h3>
                         <p className="text-gray-500 font-medium">
                             Minimum version 46
