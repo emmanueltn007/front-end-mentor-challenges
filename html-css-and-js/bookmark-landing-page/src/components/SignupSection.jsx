a7fa87ec14a12ff4becbb7c82800a980975da48d
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-22 13:36:01 +0200

    Made changes to the styling in SignupSection

--- html-css-and-js/bookmark-landing-page/src/components/SignupSection.jsx
+++ html-css-and-js/bookmark-landing-page/src/components/SignupSection.jsx
@@ -1,13 +1,13 @@
 function SignupSection () {
     return (
-        <section className="bg-[hsl(231,69%,60%)] flex flex-col items-center gap-4">
-            <p className="text-white">
+        <section className="bg-[hsl(231,69%,60%)] flex flex-col items-center gap-4 pl-8 pr-8 pt-16 pb-16">
+            <p className="text-xs text-white font-medium">
                 35,000+ ALREADY JOINED
             </p>
             <h2 className="text-white font-semibold text-2xl">
                 Stay up-to-date with what we're doing
             </h2>
-            <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col md:flex-row gap-4 max-md:w-full">
                 <input 
                     className="bg-white pt-2 pb-2 pl-2 pr-8 rounded-md"
                     type="text" 
