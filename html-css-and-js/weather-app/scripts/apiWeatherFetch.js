44ebc788c4d4541193c3adaf4d04c0c3c685a8dd
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-27 21:59:50 +0200

    Restarted the weather app with react

--- html-css-and-js/weather-app/scripts/apiWeatherFetch.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true`;
-
-fetch(apiUrl)
- .then(res => res.json())
- .then(data => {
-    console.log(data);
- });
