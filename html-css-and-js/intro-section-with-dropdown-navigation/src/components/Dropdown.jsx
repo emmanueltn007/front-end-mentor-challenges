cc54077a7de94dff99fe111cda2815d3c93d5a1d
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-27 19:18:59 +0200

    Resized the arrow icons

--- html-css-and-js/intro-section-with-dropdown-navigation/src/components/Dropdown.jsx
+++ html-css-and-js/intro-section-with-dropdown-navigation/src/components/Dropdown.jsx
@@ -6,12 +6,10 @@ function Dropdown ({ label, isOpen, onToggle, children }) {
         >
             <div className="flex gap-4 items-center">
                 <span className={`cursor-pointer  hover:text-[hsl(0,0%,8%)] ${isOpen ? "text-[hsl(0,0%,8%)]" : ""}`}>{label}</span>
-                <svg 
+                <img 
                     className={`w-3 h-2 text-[hsl(0,0%,41%)] transition duration-150 ease-in-out ${isOpen ? "rotate-180 text-[hsl(0,0%,8%)]" : ""}`}
-                    width="10" height="6" xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path stroke="currentColor" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/>
-                </svg>
+                    src="/images/icon-arrow-down.svg" alt="arrow icon"
+                />
             </div>
             {isOpen && children}
         </li>
