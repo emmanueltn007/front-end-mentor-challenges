994c03f04c89eeee2c4fb99ae0eeab38eb0e9407
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-27 19:07:45 +0200

    Removed fill in he dropdown image

--- html-css-and-js/intro-section-with-dropdown-navigation/src/components/Dropdown.jsx
+++ html-css-and-js/intro-section-with-dropdown-navigation/src/components/Dropdown.jsx
@@ -5,12 +5,13 @@ function Dropdown ({ label, isOpen, onToggle, children }) {
             className="cursor-pointer flex flex-col gap-4"
         >
             <div className="flex gap-4 items-center">
-                <span className="cursor-pointer hover:text-[hsl(0,0%,8%)] transition duration-150 ease-in-out">{label}</span>
-                <img 
-                    className={`w-auto h-2 transition duration-150 ease-in-out ${isOpen ? "rotate-180" : ""}`}
-                    src="/images/icon-arrow-down.svg" 
-                    alt="arrow icon" 
-                />
+                <span className={`cursor-pointer  hover:text-[hsl(0,0%,8%)] ${isOpen ? "text-[hsl(0,0%,8%)]" : ""}`}>{label}</span>
+                <svg 
+                    className={`w-3 h-2 text-[hsl(0,0%,41%)] transition duration-150 ease-in-out ${isOpen ? "rotate-180 text-[hsl(0,0%,8%)]" : ""}`}
+                    width="10" height="6" xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path stroke="currentColor" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/>
+                </svg>
             </div>
             {isOpen && children}
         </li>
